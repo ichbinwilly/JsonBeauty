34c12740404731a5d7441b5734e1ef3c410cac12
Willy.Hsu <Willy.hsu@deltaww.com>
2018-07-16 19:50:56 +0800

    first commit

---
+++ JsonBeauty/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace JsonBeauty
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtMsg = new System.Windows.Forms.TextBox();
+            this.btnBeautify = new System.Windows.Forms.Button();
+            this.webKitBrowser1 = new WebKit.WebKitBrowser();
+            this.SuspendLayout();
+            // 
+            // txtMsg
+            // 
+            this.txtMsg.Location = new System.Drawing.Point(50, 12);
+            this.txtMsg.Name = "txtMsg";
+            this.txtMsg.Size = new System.Drawing.Size(229, 22);
+            this.txtMsg.TabIndex = 5;
+            // 
+            // btnBeautify
+            // 
+            this.btnBeautify.Location = new System.Drawing.Point(294, 12);
+            this.btnBeautify.Name = "btnBeautify";
+            this.btnBeautify.Size = new System.Drawing.Size(75, 23);
+            this.btnBeautify.TabIndex = 4;
+            this.btnBeautify.Text = "Beautify";
+            this.btnBeautify.UseVisualStyleBackColor = true;
+            this.btnBeautify.Click += new System.EventHandler(this.btnBeautify_Click);
+            // 
+            // webKitBrowser1
+            // 
+            this.webKitBrowser1.BackColor = System.Drawing.Color.White;
+            this.webKitBrowser1.Location = new System.Drawing.Point(50, 52);
+            this.webKitBrowser1.Name = "webKitBrowser1";
+            this.webKitBrowser1.Size = new System.Drawing.Size(319, 255);
+            this.webKitBrowser1.TabIndex = 3;
+            this.webKitBrowser1.Url = null;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(415, 331);
+            this.Controls.Add(this.txtMsg);
+            this.Controls.Add(this.btnBeautify);
+            this.Controls.Add(this.webKitBrowser1);
+            this.Name = "Form1";
+            this.Text = "JSON Beauty";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtMsg;
+        private System.Windows.Forms.Button btnBeautify;
+        private WebKit.WebKitBrowser webKitBrowser1;
+
+    }
+}
+
---
+++ JsonBeauty/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace JsonBeauty
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.Load += new EventHandler(Form1_Load);
+        }
+
+        private void btnBeautify_Click(object sender, EventArgs e)
+        {
+            this.Invoke(new MethodInvoker(() =>
+            {
+                var sendMsg = txtMsg.Text;
+                webKitBrowser1.StringByEvaluatingJavaScriptFromString("beautify('" + txtMsg.Text + "')");
+            }));
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            var reqUrl = "file:///" + Application.StartupPath + "\\req.html";
+            reqUrl = reqUrl.Replace("\\", "/").Replace(" ", "%20");
+            webKitBrowser1.Navigate(reqUrl);
+        }
+    }
+}
